8c258663ebdce2f5bc7573c8cdc911b7bbbb2a12
krish03 <161813058+krishw30@users.noreply.github.com>
2026-06-19 11:22:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.1 MB (37.85%)

---
+++ my-solutions/0125-valid-palindrome/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int n = s.length() - 1;
+        
+        while (i < n) {
+            if (!isalnum(s[i])) {
+                i++;
+                continue;
+            }
+            if (!isalnum(s[n])) {
+                n--;
+                continue;
+            }
+            if (tolower(s[i]) != tolower(s[n])) {
+                return false;
+            }
+            i++;
+            n--;
+        }
+        return true;
+    }
+};
+
