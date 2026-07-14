849fc8b6218461bbb49d1d59979368c2e7939027
krish03 <161813058+krishw30@users.noreply.github.com>
2026-07-14 15:45:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26.2 MB (68.92%)

--- my-solutions/0540-single-element-in-a-sorted-array/solution.cpp
+++ my-solutions/0540-single-element-in-a-sorted-array/solution.cpp
@@ -2,36 +2,42 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        
-        // 1. The Boundary Guards (Protects against out-of-bounds)
-        if (n == 1) return nums[0];
-        if (nums[0] != nums[1]) return nums[0];
-        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
-        
-        // 2. The Search Space (Locked inward to prevent segmentation faults)
-        int low = 1;
-        int high = n - 2;
-        
-        // 3. The Engine
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            // Rule 1: The Target
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
-                return nums[mid];
+        if(n==1)return nums[0];
+         if(nums[0]!=nums[1]){
+                return nums[0];
             }
-            
-            // Rule 2: The Left Side (Even/Odd pairs)
-            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || 
-                (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
-                low = mid + 1; // Jump Right
-            } 
-            // Rule 3: The Right Side (Odd/Even pairs)
-            else {
-                high = mid - 1; // Jump Left
-            }
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
         }
-        
-        return -1;
+        int low=1;
+        int high=n-2;
+        int answer=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            // if mid is the sec. ele. of the pair
+            if(nums[mid]==nums[mid-1]&&mid%2==1){
+                low=mid+1;
+                continue;
+            }
+            if(nums[mid]==nums[mid+1]&&mid%2==0){
+                low=mid+1;
+                continue;
+            }
+            if(nums[mid]==nums[mid-1]&&mid%2==0){
+                high=mid-1;
+                continue;
+            }
+            if(nums[mid]==nums[mid+1]&&mid%2==1){
+                high=mid-1;
+                continue;
+            }
+            else{
+                answer= nums[mid];
+                break;
+            }
+
+           
+        }
+        return answer;
     }
 };
