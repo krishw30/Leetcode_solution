786a040bd21e8f45094e2fb4c19f0bbdc97f58f9
krish03 <161813058+krishw30@users.noreply.github.com>
2026-07-21 16:45:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (85.22%)

---
+++ my-solutions/2032-largest-odd-number-in-string/solution.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+class Solution {
+public:
+    std::string largestOddNumber(std::string num) {
+        for (int i = num.size() - 1; i >= 0; --i) {
+            if ((num[i] - '0') % 2 != 0) {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
+    }
+};
+
